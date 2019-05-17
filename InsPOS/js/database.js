58ba0e08da520fc91d23ec71f616497e9f9ff912
shehan <shehaanc@gmail.com>
2019-05-17 07:37:18 +0530

    add the data base js

---
+++ InsPOS/js/database.js
@@ -0,0 +1,48 @@
+var Courses=[{
+    id:"C1",
+    name:"abc",
+   description:"def",
+    duration:"12"
+},
+    {
+
+        id:"C1",
+        name:"abc",
+        description:"def",
+        duration:"12"
+    }];
+
+var Batches=[{
+    id:"B1",
+    name:"def",
+    courseid:"c1",
+    courcename:"abc"
+
+},
+    {
+        id:"B2",
+        name:"abc",
+        courseid:"c2",
+        courcename:"def"
+    }];
+
+var register = [
+    {
+        oid:'Order001',
+        date:'10-05-2019',
+        cusId:'C002',
+        total:'12,000'
+    },
+    {
+        oid:'Order002',
+        date:'13-05-2019',
+        cusId:'C003',
+        total:'32000'
+    },
+    {
+        oid:'Order003',
+        date:'13-05-2019',
+        cusId:'C002',
+        total:'12000'
+    }
+];
